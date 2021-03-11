43141b1d6c33e7c43dc870b11a1e7359653f769c
chenbb <2636966033@qq.com>
2021-03-11 19:46:52 +0800

    no message

--- main.go
+++ main.go
@@ -1,16 +1,5 @@
 package main
 
-import "fmt"
-
 func main() {
-	fmt.Println("hello go ")
-}
-
-func test() {
-	fmt.Println("eeee")
-}
-func test2() {
-	fmt.Println("wwwwwww")
-	fmt.Println("wwwwwww")
 
 }
