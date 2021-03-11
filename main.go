e28705314dbc5d52891c146025214326c59588f9
chenbb <2636966033@qq.com>
2021-03-11 17:44:08 +0800

    test

--- main.go
+++ main.go
@@ -5,3 +5,15 @@ import "fmt"
 func main() {
 	fmt.Println("hello go ")
 }
+
+func test() {
+	fmt.Println("eeee")
+}
+
+func test1() {
+	fmt.Println("eeee")
+}
+
+func test2() {
+	fmt.Println("eeee")
+}
